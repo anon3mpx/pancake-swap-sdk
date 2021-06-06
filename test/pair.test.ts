7d311325f39f0c7655bfcb219ee4325ef6b08153
infvoid <infvoid.defi@yandex.ru>
2021-06-06 10:33:56 +0800

    fix unit test

--- test/pair.test.ts
+++ test/pair.test.ts
@@ -14,7 +14,7 @@ describe('Pair', () => {
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(USDC, DAI)).toEqual('0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5')
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0x5F8E04Cb6e0190921f2c050E73A5c4561f7c7157')
     })
   })
 
